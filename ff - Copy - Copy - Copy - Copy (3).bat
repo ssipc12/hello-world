73d5762503ea17331b97cb143e7b5b5e9e9b9899
mahmood <ssipc12@gmail.com>
2021-11-03 15:14:15 +0330

    your description of what to do

---
+++ ff - Copy - Copy - Copy - Copy (3).bat
@@ -0,0 +1,10 @@
+git add .
+
+git commit -m "your description of what to do"
+
+REM git remote add origin https://github.com/ssipc12/hello-world.git
+git push --set-upstream origin main
+
+
+
+pause
