b292c40a8fd868375c94381e457f008f5e352c84
mahmood <ssipc12@gmail.com>
2021-11-03 14:55:54 +0330

    your description of what to do

--- ff.bat
+++ ff.bat
@@ -3,7 +3,7 @@ git add .
 git commit -m "your description of what to do"
 
 git remote add origin https://github.com/ssipc12/hello-world.git
-git push --set-upstream origin master
+git push --set-upstream origin main
 
 
 
